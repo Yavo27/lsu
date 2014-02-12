d43cdde44d2efffd8da559f56d5715752d6ce035
Yon <yon@yon-VirtualBox.(none)>
2014-02-12 15:10:12 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lsu::Application.config.secret_key_base = 'b98ae448c3cb2b32d47983e68acd7abaf1334a8321b19231192fd4073110e512937756e68c2f06c47ec4d638087599b69ba202afb7cb4929945e5d2d8d9edcae'
